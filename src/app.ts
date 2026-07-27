998f9f6075d8304474b81eaa401e2b4cc321f6ff
Addam Cosmo <addamcosmo2000@gmail.com>
2026-07-26 21:49:51 -0300

    validação para expiração de url, e validação para contagem dos cliqes

--- src/app.ts
+++ src/app.ts
@@ -10,37 +10,40 @@ app.get('/',(req,reply)=>{
     return reply.send('Ola mundo')
 })
 
-app.post('/url',async(req,reply)=>{
-  
-  const {originalUrl}  = req.body as {originalUrl:string}
+app.post('/url', async (req, reply) => {
 
-  if(originalUrl === ''){
+  const { originalUrl, expiresAt } = req.body as { originalUrl: string, expiresAt?:Date } 
+
+  if (originalUrl === '') {
     return reply.send('A url não pode ser vazia')
   }
 
   const urlunica = await prisma.url.findFirst({
-      where:{originalUrl}
+    where: { originalUrl }
   })
 
-  if(urlunica){
+  if (urlunica) {
     return reply.send(`Já existe um encurtador para essa url: http://localhost:3000/${urlunica.shortCode}`)
   }
 
-  // no meu banco a url reduzida recebe a função reduzida
- const shortCode = generateShortCode()
+  let shortCode: string;
+  let existe;
+
+  do {
+    shortCode = generateShortCode();
+    existe = await prisma.url.findUnique({ where: { shortCode } });
+  } while (existe !== null);
 
   const createdUrl = await prisma.url.create({
     data: {
       shortCode: shortCode,
-      originalUrl: originalUrl
+      originalUrl: originalUrl,
+      expiresAt 
     }
   });
 
   return reply.send(`http://localhost:3000/${shortCode}`)
 
-
-
-  
 })
 
 
@@ -52,12 +55,22 @@ app.get('/:shortCode',async(req,reply)=>{
   })
 
   if(!urlcompleta){
-    return  reply.status(404).send('Url não encontrada , primeiro é encesssario gerar ')
+    return  reply.status(404).send('Url não encontrada , primeiro é necesssario gerar ')
   }
 
-  
+  await prisma.url.update({
+    where:{shortCode},
+    data:{
+      clicks:{increment:1}
+    }
+  })
+
+  if(urlcompleta.expiresAt && urlcompleta.expiresAt < new Date()){
+    return reply.status(410).send('Este link expirou')
+  }
 
   return reply.redirect(urlcompleta.originalUrl)
+
 })
 
 
